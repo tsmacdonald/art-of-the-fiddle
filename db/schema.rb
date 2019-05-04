e249044c21acecb11a99f8d669d7d9dea6347f03
Tim Macdonald <tsmacdonald@gmail.com>
2019-05-04 11:58:49 -0500

    Add Tag and Tagging models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_164026) do
+ActiveRecord::Schema.define(version: 2019_05_04_165236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,4 +45,21 @@ ActiveRecord::Schema.define(version: 2019_05_04_164026) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.bigint "artwork_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artwork_id"], name: "index_taggings_on_artwork_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "taggings", "artworks"
+  add_foreign_key "taggings", "tags"
 end
